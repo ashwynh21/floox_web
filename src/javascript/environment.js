1e4a58a9ebdb1314dbd357e5c5400d1310104985
Ashwyn Horton <ashwynh21@gmail.com>
2021-05-20 08:58:04 +0200

    adjusted all html files to support mobile view.

--- src/javascript/environment.js
+++ src/javascript/environment.js
@@ -1,4 +1,4 @@
-const root = 'http://localhost:5000';
+const root = 'http://34.71.249.95:5000';
 const configuration = {
     client_id: '221425552189-arh623bv6n39sbvqdv0lgqh9dn7imr98.apps.googleusercontent.com',
     cookiepolicy: 'single_host_origin',
