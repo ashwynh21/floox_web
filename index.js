cb1120952e31ed476b567176e5bc473d788ec7d4
Ashwyn Horton <ashwynh21@gmail.com>
2021-05-21 15:11:54 +0200

    Okay, setup with SSL server is done, getting rid of the old server completely so that it only runs on the secured server with our SSL certificate.

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
+//
+const cert = fs.readFileSync('/etc/letsencrypt/live/www.floox.co.za/fullchain.pem', 'utf8').toString();
+const key = fs.readFileSync('/etc/letsencrypt/live/www.floox.co.za/privkey.pem', 'utf8').toString();
+//
+const server = https.createServer(
+    {
+        cert,
+        key,
+    },
+    app,
+);
 
 app.use(express.static(`${__dirname}/build/default`));
 
-app.listen(80, () => console.log('listening on port 80'));
+server.listen(443, () => console.log('listening on port 443'));
--- src/javascript/environment.js
+++ src/javascript/environment.js
@@ -1,4 +1,4 @@
-const root = 'http://localhost:5000';
+const root = 'http://www.floox.co.za:5000';
 const configuration = {
     client_id: '221425552189-arh623bv6n39sbvqdv0lgqh9dn7imr98.apps.googleusercontent.com',
     cookiepolicy: 'single_host_origin',
