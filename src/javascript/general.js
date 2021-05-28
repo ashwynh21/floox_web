95b1d3e3871d9b1c68b4b73e307d66c41fc73b44
Ashwyn Horton <ashwynh21@gmail.com>
2021-05-28 20:27:26 +0200

    theres a small amount of sync loss, making an onload event handler to mitigate the load time issue

--- src/javascript/general.js
+++ src/javascript/general.js
@@ -5,45 +5,48 @@
 
 // we define a constant for the server url.
 
-((client) => {
-    /*
-    * So here  we will configure the events that we will want to listen to.
-    * */
-    const counter = document.querySelector('#active_clients');
-    const profit = document.querySelector('#total_profit');
-    const won = document.querySelector('#won');
-    const lost = document.querySelector('#lost');
-
-    client.on('general/summary', (message) => {
+window.onload = () => {
+    ((client) => {
         /*
-        * so here we simply get the dom object that we will fill with
-        * this data.
+        * So here  we will configure the events that we will want to listen to.
         * */
-        counter.innerHTML = message.data.clients;
-        profit.innerHTML = tocurrency(message.data.profit);
-        won.innerHTML = message.data.won;
-        lost.innerHTML = message.data.lost;
+        const counter = document.querySelector('#active_clients');
+        const profit = document.querySelector('#total_profit');
+        const won = document.querySelector('#won');
+        const lost = document.querySelector('#lost');
 
-        if(counter.value !== message.data.clients) {
-            nudge(counter.parentNode);
-        }
-        if(profit.value !== message.data.profit) {
-            nudge(profit.parentNode);
-        }
-        if(won.value !== message.data.won) {
-            nudge(won.parentNode);
-        }
-        if(lost.value !== message.data.lost) {
-            nudge(lost.parentNode);
-        }
+        client.on('general/summary', (message) => {
+            console.log(message);
+            /*
+            * so here we simply get the dom object that we will fill with
+            * this data.
+            * */
+            counter.innerHTML = message.data.clients;
+            profit.innerHTML = tocurrency(message.data.profit);
+            won.innerHTML = message.data.won;
+            lost.innerHTML = message.data.lost;
 
-        won.value = message.data.won;
-        lost.value = message.data.lost;
-        counter.value = message.data.clients;
-        profit.value = message.data.profit;
-    });
+            if(counter.value !== message.data.clients) {
+                nudge(counter.parentNode);
+            }
+            if(profit.value !== message.data.profit) {
+                nudge(profit.parentNode);
+            }
+            if(won.value !== message.data.won) {
+                nudge(won.parentNode);
+            }
+            if(lost.value !== message.data.lost) {
+                nudge(lost.parentNode);
+            }
 
-})(socket);
+            won.value = message.data.won;
+            lost.value = message.data.lost;
+            counter.value = message.data.clients;
+            profit.value = message.data.profit;
+        });
+
+    })(socket);
+}
 
 function nudge(target) {
     anime({
